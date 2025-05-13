efec7954461f49832dd929638ea6486e344a72e9
waqas <waqas@gmail.com>
2025-05-13 11:12:42 -0700

    aa

---
+++ resources/views/blog-details.blade.php
@@ -0,0 +1,250 @@
+@extends('layout')
+@section('title', 'DJOZ | contact')
+
+@section('pageContent')
+<br><br><br>
+    <!-- Breadcrumb Begin -->
+    <div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="#"><i class="fa fa-home"></i> Home</a>
+                        <a href="#">Blog</a>
+                        <span>Guidelines for music festival event organisers: music festival harm reduction - December
+                            2019</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+    <!-- Blog Details Section Begin -->
+    <section class="blog-details spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="blog__details__content">
+                        <div class="blog__details__item">
+                            <div class="blog__details__item__pic set-bg" data-setbg="img/blog/details/details-pic.jpg">
+                                <a href="#"><i class="fa fa-share-alt"></i></a>
+                            </div>
+                            <div class="blog__details__item__text">
+                                <span>Music festival</span>
+                                <h3>Guidelines for music festival event organisers: music festival harm reduction -
+                                    December 2019</h3>
+                                <div class="blog__details__item__widget">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <ul>
+                                                <li>By <span>Erna O’Conner</span></li>
+                                                <li>Dec 17, 2019</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <ul>
+                                                <li>230 Views</li>
+                                                <li>18 Comments</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog__details__desc">
+                            <p>I feel as though a lot of people who read my articles are under the impression that I am
+                                suggesting eventually someone is going to come into their life and every puzzle piece
+                                will simply pop into place. Doves will fly out from behind you, a chorus will follow you
+                                around on every date, you will find a bag with 10 million dollars in the street, which
+                                you will use to move to Pleasantville, USA.</p>
+                            <p>You need mutual respect, compromise, sacrifice, understanding, the willingness to work at
+                                it and stand by him or her when times get rough. You need to be willing to be by their
+                                side not only during the bright days but also during the dark ones that Love is not all
+                                you need.</p>
+                        </div>
+                        <div class="blog__details__quote">
+                            <p>None of them will tell you that they will stay together forever because it’s easy. None
+                                of them will tell you that they pledged their life.</p>
+                            <h6>OSCAR HUDSPETH</h6>
+                            <i class="fa fa-quote-right"></i>
+                        </div>
+                        <div class="blog__details__desc">
+                            <p>You are committing to someone’s whole self. You are not just committing to them under the
+                                condition that they stay young and beautiful — because they will not. And neither will
+                                you. You are not just committing to them until someone better comes along while neither
+                                they nor your relationship is perfect.</p>
+                            <p>This is the person you want to be with. You are committing to their very being. To the
+                                idea that the two of you are</p>
+                        </div>
+                        <div class="blog__details__tags">
+                            <a href="#">Music</a>
+                            <a href="#">Festival</a>
+                            <a href="#">Tinternational</a>
+                            <a href="#">Event</a>
+                        </div>
+                        <div class="blog__details__option">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <a href="#" class="blog__option__btn">
+                                        <h6 class="option__btn__name"><i class="fa fa-angle-left"></i> Previous posts
+                                        </h6>
+                                        <div class="blog__option__btn__item">
+                                            <div class="blog__option__btn__pic">
+                                                <img src="img/blog/details/blog-change.jpg" alt="">
+                                            </div>
+                                            <div class="blog__option__btn__text">
+                                                <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                                <span>Dec 17, 2019</span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <a href="#" class="blog__option__btn blog__option__btn--next">
+                                        <h6 class="option__btn__name">Next posts <i class="fa fa-angle-right"></i></h6>
+                                        <div class="blog__option__btn__item">
+                                            <div class="blog__option__btn__pic">
+                                                <img src="img/blog/details/blog-change.jpg" alt="">
+                                            </div>
+                                            <div class="blog__option__btn__text">
+                                                <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                                <span>Dec 17, 2019</span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog__details__services">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="blog__details__services__title">
+                                        <h4>You may also like</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="blog__details__services__item">
+                                        <div class="blog__details__services__item__pic">
+                                            <img src="img/blog/details/bs-1.jpg" alt="">
+                                        </div>
+                                        <div class="blog__details__services__item__text">
+                                            <h6><a href="#">The Best Performances At The EFG London…</a></h6>
+                                            <span>Dec 06, 2019</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="blog__details__services__item">
+                                        <div class="blog__details__services__item__pic">
+                                            <img src="img/blog/details/bs-2.jpg" alt="">
+                                        </div>
+                                        <div class="blog__details__services__item__text">
+                                            <h6><a href="#">Indigo Girls to Headline 2020 Amplify Decatur…</a></h6>
+                                            <span>Dec 06, 2019</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="blog__details__services__item">
+                                        <div class="blog__details__services__item__pic">
+                                            <img src="img/blog/details/bs-3.jpg" alt="">
+                                        </div>
+                                        <div class="blog__details__services__item__text">
+                                            <h6><a href="#">Here’s All The Highlights From This Year’s…</a></h6>
+                                            <span>Dec 06, 2019</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog__details__form">
+                            <div class="blog__details__form__title">
+                                <h4>Leave a comment</h4>
+                            </div>
+                            <form action="#">
+                                <div class="input__list">
+                                    <input type="text" placeholder="Name">
+                                    <input type="text" placeholder="Email">
+                                    <input type="text" placeholder="Website">
+                                </div>
+                                <textarea placeholder="Comment"></textarea>
+                                <button type="submit" class="site-btn">SEND MESSAGE</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="blog__sidebar">
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Subscribe newsletter</h4>
+                            </div>
+                            <p>Ipsum dolor sit amet, adipiscing elit, sed eiusmod </p>
+                            <form action="#">
+                                <input type="text" placeholder="Name">
+                                <input type="text" placeholder="Email">
+                                <button type="submit" class="site-btn">Subscribe</button>
+                            </form>
+                        </div>
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Social media</h4>
+                            </div>
+                            <div class="blog__sidebar__social">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </div>
+                        </div>
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Recent posts</h4>
+                            </div>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-1.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-2.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-3.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-4.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Details Section End -->
+@endsection
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,230 @@
+@extends('layout')
+@section('title', 'DJOZ | contact')
+
+@section('pageContent')
+<br><br><br>
+    <!-- Breadcrumb Begin -->
+    <div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="#"><i class="fa fa-home"></i> Home</a>
+                        <span>Blog</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+    <!-- Blog Section Begin -->
+    <section class="blog spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="section-title">
+                        <h2>Latest posts</h2>
+                        <h1>Music blog</h1>
+                    </div>
+                    <div class="blog__large">
+                        <div class="blog__large__pic set-bg" data-setbg="img/blog/large-item.jpg">
+                            <a href="#"><i class="fa fa-share-alt"></i></a>
+                        </div>
+                        <div class="blog__large__text">
+                            <span>Music festival</span>
+                            <h4>Guidelines for music festival event organisers: music festival harm reduction - December
+                                2019</h4>
+                            <p>Lorem ipsum dolor consectetur adipiscing eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <div class="blog__large__widget">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <ul>
+                                            <li>By <span>Erna O’Conner</span></li>
+                                            <li>Dec 17, 2019</li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <ul class="right__widget">
+                                            <li>230 Views</li>
+                                            <li>18 Comments</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-1.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>World Music Festival | Free Events & Concerts in Chicago</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-2.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>How ROQU Media and The Manual London staged Saudi…</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-3.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>2019 Festival of the Sun music event, Port Macquarie</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-4.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>Taylor Swift to Headline Glaston Music Festival</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-5.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>24-hour festival Inner City Electronic returns</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-6.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text">
+                                    <span>Music festival</span>
+                                    <h5>Lost Paradise festival cancelled amid bushfire danger</h5>
+                                    <ul>
+                                        <li>By <span>Erna O’Conner</span></li>
+                                        <li>Dec 17, 2019</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="pagination__links blog__pagination">
+                                <a href="#">1</a>
+                                <a href="#">2</a>
+                                <a href="#">3</a>
+                                <a href="#">Next</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="blog__sidebar">
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Subscribe newsletter</h4>
+                            </div>
+                            <p>Ipsum dolor sit amet, adipiscing elit, sed eiusmod </p>
+                            <form action="#">
+                                <input type="text" placeholder="Name">
+                                <input type="text" placeholder="Email">
+                                <button type="submit" class="site-btn">Subscribe</button>
+                            </form>
+                        </div>
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Social media</h4>
+                            </div>
+                            <div class="blog__sidebar__social">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </div>
+                        </div>
+                        <div class="blog__sidebar__item">
+                            <div class="blog__sidebar__title">
+                                <h4>Recent posts</h4>
+                            </div>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-1.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-2.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-3.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                            <a href="#" class="recent__item">
+                                <div class="recent__item__pic">
+                                    <img src="img/blog/br-4.jpg" alt="">
+                                </div>
+                                <div class="recent__item__text">
+                                    <h6>2020 Bendigo Blues and Roots Music Festival…</h6>
+                                    <span>Dec 17, 2019</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Section End -->
+@endsection
